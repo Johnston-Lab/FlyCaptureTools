c8d6ac5889ebb7b9e655991d7010fd8e1d4edb9f
David Watson <david.watson@nottingham.ac.uk>
2020-12-10 17:22:19 +0000

    Strip trailing spaces

--- extract_embedded_image_info.py
+++ extract_embedded_image_info.py
@@ -45,63 +45,63 @@ def extractInfo(frame, properties):
         Extracted values for each property, keyed by property name.
         Timestamp and ROIPosition properties are represented as dicts of
         multiple values, other properties are represented directly.
-    """    
+    """
     # Use all properties if requested
     if (properties == 'all') or ('all' in properties):
         properties = PROPERTIES
-        
+
     # Convert image to uint8 if necessary
     if not frame.dtype == 'uint8':
         warnings.warn('Converting frame to uint8')
         frame = frame.dtype()
-    
+
     # Assert frame is grayscale
     if frame.ndim == 3:
-        for ii in range(1, frame.shape[2]):  
+        for ii in range(1, frame.shape[2]):
             if not np.all(frame[...,0] == frame[...,ii]):
                 raise ValueError('Image must be grayscale')
         frame = frame[...,0]
-        
+
     # Assert requested fields are valid names
     for prop in properties:
         if prop not in PROPERTIES:
             raise ValueError(f'\{property}\ not a valid proptery name')
-        
+
     # Pre-allocate dict for storing results
     res = {}
-        
-    # We now need to check properties IN ORDER    
+
+    # We now need to check properties IN ORDER
     idx = 0
     for prop in PROPERTIES:
         # Skip properties not requested
         if prop not in properties:
             continue
-        
+
         # Extract pixels and convert to binary string
         data = frame[0,idx:(idx+4)]
         b = ''.join(np.binary_repr(x, width=8) for x in data)
         idx += 4
-        
+
         # Timestamps need some special handling
         if prop == 'timestamp':
             second_count = int(b[:7], 2)
             cycle_count = int(b[7:20], 2)
             cycle_offset = int(b[20:], 2)
             cycle_offset_as_count = cycle_offset / 3072
-            cycle_seconds = (cycle_count + cycle_offset_as_count) / 8000            
+            cycle_seconds = (cycle_count + cycle_offset_as_count) / 8000
             res['timestamp'] = {'second_count':second_count,
                                 'cycle_count':cycle_count,
                                 'cycle_offset':cycle_offset,
                                 'cycle_seconds':cycle_seconds}
-            
+
         # ROI position also needs special handling
         elif prop == 'ROIPosition':
             res['ROIPosition'] = {'left':int(b[:2], 2), 'top':int(b[2:], 2)}
-            
+
         # All other fields, just convert straight away
         else:
             res[prop] = int(b, 2)
-            
+
     # Return
     return res
 
@@ -124,14 +124,14 @@ def processClip(filepath, properties):
     """
     # Load clip
     clip = VideoFileClip(filepath)
-    
-    # Loop frames, yield info for each    
+
+    # Loop frames, yield info for each
     for frame in clip.iter_frames():
         yield extractInfo(frame, properties)
 
-            
+
 if __name__ == '__main__':
-    
+
     # Parse args
     parser = argparse.ArgumentParser(usage=__doc__,
                                      formatter_class=CustomFormatter)
@@ -142,48 +142,48 @@ if __name__ == '__main__':
                         help='List of properties to extract from frames')
     parser.add_argument('-o', '--output', nargs='+',
                         help='Path to output csv file(s)')
-    
+
     args = parser.parse_args()
     infiles = args.input
     properties = args.properties
     outfiles = args.output
-    
+
     if outfiles is None:
         outfiles = [os.path.splitext(infile)[0] + '-embedded_info.csv' \
                     for infile in infiles]
     elif len(outfiles) == 1:
         outfiles = outfiles * len(infiles)
-        
+
     # Loop files
     for infile, outfile in zip(infiles, outfiles):
         print(infile)
-        
+
         # Open outfile
         fd = open(outfile, 'w')
-        
+
         # Process & write out
         res = processClip(infile, properties)
         for i, row in enumerate(res):
-            
+
             # Timestamps and ROIPosition need a bit of re-formatting
             if 'timestamp' in row.keys():
                 for k, val in row['timestamp'].items():
                     row[f'timestamp.{k}'] = val
                 row.pop('timestamp')
-                
+
             if 'ROIPosition' in row.keys():
                 for k, val in row['ROIPosition'].items():
                     row[f'ROIPosition.{k}'] = val
                 row.pop('ROIPosition')
-                
+
             # On 1st iter, open csv writer and write headers
             if i == 0:
                 writer = csv.DictWriter(fd, fieldnames=row.keys(),
                                         delimiter=',', lineterminator='\n')
                 writer.writeheader()
-                
+
             # Write data
             writer.writerow(row)
-            
+
         # Close outfile and finish
-        fd.close()    
+        fd.close()
